267995b02d56aceb5815c221ac01ea9ba93b8167
Charalampos Leontidis <chleontidis@yahoo.com>
2025-06-13 15:42:34 +0300

    Initial commit

---
+++ js/i18n.js
@@ -0,0 +1,108 @@
+const translations = {
+    en: {
+        About: "About",
+        about: "about",
+        Discography: "Discography",
+        Idiorhythmo: "Idiorhythmo",
+        Events: "Events",
+        Archive: "Archive",
+        Upcoming: "Upcoming",
+        Contact: "Contact",
+        Un: "Un",
+        holy_Mustache: "holy Mustache",
+        "who-am-i-content": "Antonis Leontidis was born in 1996 in Athens. He hails from the village of Margarites in Mylopotamos and from Pontus. He began learning the Cretan lyra at the age of nine with teacher Panos Gkotsis and, for many years thereafter, with teacher Giorgos Skordalos. He has studied under many distinguished teachers of Cretan music and broader Western and Eastern musical traditions. He has attended lessons with: Ross Daly, Kelly Thoma, Chihan Turkoglu, Karolos Kouklakis, Irini Derempei, Maria Koti, Paris Perisinakis, Mayu Shviro, Vangelis Karipis, Ioannis Idomenaeus, and Sina Danesh. He has also participated in music seminars at the Labyrinth Musical Workshop with: Hooshang Farahani, Martha Mavroidi, Stelios Petrakis, Zohar Fresco, Giorgos Xylouris (Psarogiorgis), and at the Meronas Musical School with Dimitris Sgouros and Stratis Skarakis. He holds a diploma in music theory from the National Conservatory and has studied Byzantine Music under Giannis Arvanitis and Michalis Kottis. Since 2014, he has worked professionally as a musician, performing in Crete, Athens, and abroad — in Italy, Spain, the Netherlands, Belgium, Germany, and France. Since 2015, he has been teaching the techniques and repertoire of the Cretan lyra to both children and adults. He has also worked as a researcher for the organization Istorima, where he conducted 24 life-history interviews with older musicians, dancers, instrument makers, and singers from various regions of western and eastern Crete.",
+        "who-am-i": "Who Am I",
+        Works: "Works",
+        "works-content-1": "He has composed the music for the documentaries:",
+        "documentary-1": "«Harder years, happier years» 2018 - Produced by Social Cooperative Enterprise «Rodakas»",
+        "documentary-2": "«The threshold of memory» 2019 - Produced by Social Cooperative Enterprise «Rodakas»",
+        "documentary-3": "«Daniel, a French ceramist in Crete» 2021 - Short documentary by director Thodoris Markou",
+        "documentary-4": "«La route de l’argile» 2025 - by Sarah Georgel-Debedde and Kristen Durand.",
+        "works-content-2": "In 2024, he created the musical ensemble «Idiorrhythmo» in Margarites, Mylopotamos, made up of 25 amateur and professional musicians, through which he researches recordings of Cretan music from the early to mid-20th century, which are then orchestrated and presented to the public.",
+        "works-content-3": "In 2021, he released his first solo album titled «Kryfo», containing his own musical compositions based on the Cretan music motif with mantinades from the collection «Cretan Mantinades» (1933) by folklorist Maria Lioudaki. The album features Yiannis Linardakis on lute and vocals.",
+        "works-content-4": "In 2024, he released his second album titled «Asprougas», which includes six original instrumental compositions for Cretan lyra, double bass, and cello. The album features the musicians: Juliette Weiss, Stefanos Floras, Sol Ligertwood."
+    },
+    fr: {
+        About: "À propos",
+        about: "à propos",
+        Discography: "Discographie",
+        Idiorhythmo: "Idiorhythmo",
+        Events: "Événements",
+        Archive: "Archives",
+        Upcoming: "À venir",
+        Contact: "Contact",
+        Un: "Im",
+        holy_Mustache: "pie Mustache",
+        "who-am-i-content": "Antonis Leontidis est né en 1996 à Athènes. Il est originaire du village de Margarites à Mylopotamos et du Pont. Il a commencé à apprendre la lyra crétoise à l'âge de neuf ans avec le professeur Panos Gkotsis, puis pendant plusieurs années avec le professeur Giorgos Skordalos. Il a été formé par de nombreux enseignants remarquables dans le domaine de la musique crétoise ainsi que dans les traditions musicales occidentales et orientales plus larges. Il a suivi des cours avec : Ross Daly, Kelly Thoma, Chihan Turkoglu, Karolos Kouklakis, Irini Derempei, Maria Koti, Paris Perisinakis, Mayu Shviro, Vangelis Karipis, Ioannis Idomenaeus et Sina Danesh. Il a également participé à des séminaires musicaux au Laboratoire Musical Labyrinthos avec : Hooshang Farahani, Martha Mavroidi, Stelios Petrakis, Zohar Fresco, Giorgos Xylouris (Psarogiorgis), ainsi qu’à l’École Musicale de Méronas avec Dimitris Sgouros et Stratis Skarakis. Il est titulaire d’un diplôme de théorie musicale du Conservatoire National et a suivi des cours de musique byzantine avec Giannis Arvanitis et Michalis Kottis. Depuis 2014, il travaille professionnellement en tant que musicien, se produisant en Crète, à Athènes, ainsi qu’à l’étranger : en Italie, en Espagne, aux Pays-Bas, en Belgique, en Allemagne et en France. Depuis 2015, il enseigne les techniques et le répertoire de la lyra crétoise aux enfants et aux adultes. Il a également travaillé comme chercheur pour l'organisation Istorima, où il a mené 24 entretiens – histoires de vie – avec d’anciens musiciens, danseurs, luthiers, chanteurs et chanteuses de différentes régions de Crète occidentale et orientale.",
+        "who-am-i": "Qui suis-je",
+        Works: "Œuvres",
+        "works-content-1": "Il a composé la musique pour les documentaires :",
+        "documentary-1": "«Des années plus dures, des années plus heureuses» 2018 - Production par l’entreprise sociale «Rodakas»",
+        "documentary-2": "«Le seuil de la mémoire» 2019 - Production par l’entreprise sociale «Rodakas»",
+        "documentary-3": "«Daniel, un céramiste français en Crète» 2021 - Court documentaire du réalisateur Thodoris Markou",
+        "documentary-4": "«La route de l’argile» 2025 - de Sarah Georgel-Debedde et Kristen Durand.",
+        "works-content-2": "En 2024, il a fondé l’ensemble musical «Idiorrhythmo» à Margarites, Mylopotamos, composé de 25 musiciens amateurs et professionnels, à travers lequel il explore les enregistrements de musique crétoise du début jusqu’au milieu du XXe siècle, qu’il présente ensuite orchestrés au public.",
+        "works-content-3": "En 2021, il a sorti son premier album solo intitulé «Kryfo», comprenant ses propres compositions musicales inspirées du motif crétois, avec des mantinades issues de la collection «Mantinades crétoises» (1933) de la folkloriste Maria Lioudaki. L’album inclut Yiannis Linardakis au luth et au chant.",
+        "works-content-4": "En 2024, il a sorti son deuxième album intitulé «Asprougas», contenant six compositions instrumentales originales pour lyra crétoise, contrebasse et violoncelle. Y participent les musiciens : Juliette Weiss, Stefanos Floras, Sol Ligertwood."
+    },
+    el: {
+        About: "Σχετικά",
+        about: "σχετικά",
+        Discography: "Δισκογραφία",
+        Idiorhythmo: "Ιδιόρρυθο",
+        Events: "Εκδηλώσεις",
+        Archive: "Αρχείο",
+        Upcoming: "Επερχόμενα",
+        Contact: "Επικοινωνία",
+        Un: "Αν",
+        holy_Mustache: "ιερο Μουστακι",
+        "who-am-i-content": "Ο Αντώνης Λεοντίδης γεννήθηκε το 1996 στην Αθήνα. Κατάγεται από το χωριό Μαργαρίτες Μυλοποτάμου και από τον Πόντο. Ξεκίνησε να μαθαίνει κρητική λύρα σε ηλικία εννέα ετών με δάσκαλο τον Πάνο Γκότση και στην συνέχεια, επί σειρά ετών, με δάσκαλο τον Γιώργο Σκορδαλό. Έχει μαθητεύσει με πολλούς και αξιόλογους δασκάλους και δασκάλες της κρητικής μουσικής και ευρύτερα δυτικών και ανατολικών μουσικών παραδώσεων. Έχει παρακολουθήσει μαθήματα με τους: Ross Daly, Κέλυ Θωμά, Chihan Turkoglu, Κάρολο Κουκλάκη, Ειρήνη Δερέμπεη, Μαρία Κώτη, Πάρι Περυσινάκη, Mayu Shviro, Βαγγέλη Καρίπη, Ιωάννη Ιδομενέως, Sina Danesh. Επιπλέον έχει συμμετάσχει σε μουσικά σεμινάρια, στο Μουσικό Εργαστήρι Λαβύρινθος, με τους: Hooshang Farahani, Μάρθα Μαυροειδή, Στέλιο Πετράκη, Zohar Fresco, Γιώργο Ξυλούρη (Ψαρογιώργης) και στο Μουσικό Διδασκαλείο του Μέρωνα, με τους: Δημήτρη Σγουρό και Στρατή Σκαράκη. Έχει πτυχίο θεωρίας απο το Εθνικό Ωδείο και έχει παρακολουθήσει μαθήματα Βυζαντινής Μουσικής με τον Γιάννη Αρβανίτη και τον Μιχάλη Κώττη.  Από το 2014 εργάζεται επαγγελματικά ως μουσικός με εμφανίσεις στην Κρήτη και στην Αθήνα καθώς και στο εξωτερικό στην Ιταλία, Ισπανία, Ολλανδία, Βέλγιο, Γερμανία και Γαλλία. Από το 2015 διδάσκει τις τεχνικές και το ρεπερτόριο της κρητικής λύρας σε παιδιά και ενήλικες. Έχει εργαστεί ως ερευνητής για την εταιρία «Istorima» όπου πήρε 24 συνεντεύξεις – ιστορικά ζωής από παλιούς μουσικούς, χορευτές, οργανοποιούς, τραγουδιστές και τραγουδίστριες από διαφορετικές επαρχίες της δυτικής και ανατολικής Κρήτης.",
+        "who-am-i": "Ποιος είμαι",
+        Works: "Έργα",
+        "works-content-1": "Έχει συνθέσει την μουσική για τα ντοκιμαντερ:",
+        "documentary-1": "«Πιο δύσκολα χρόνια, πιο ευτυχισμένα χρόνια» 2018 - Παραγωγή Κοιν.Σ.Επ. «Ρόδακας»",
+        "documentary-2": "«Το κατώφλι της μνήμης» 2019 - Παραγωγή Κοιν.Σ.Επ. «Ρόδακας»",
+        "documentary-3": "«Daniel, a French ceramist in Crete» 2021 - Ντοκιμαντέρ μικρού μήκους του σκηνοθέτη Θοδωρή Μάρκου",
+        "documentary-4": "«La route de l ‘ argile»2025 - των: Sarah Georgel-Debedde και Kristen Durand.",
+        "works-content-2": "Το 2024 δημιούργησε στις Μαργαρίτες Μυλοποτάμου το μουσικό σύνολο «Ιδιόρρυθμο» που αποτελείται απο 25 ερασιτέχνες και επαγγελματίες μουσικούς μέσω του οποίου ερευνά τις ηχογραφήσεις της κρητικής μουσικής των αρχών έως τα μέσα του 20ου αιώνα οι οποίες στην συνέχεια παρουσιάζονται ενορχηστρωμένες στο κοινό.",
+        "works-content-3": "Το 2021 κυκλοφόρησε το πρώτο του προσωπικό άλμπουμ με τίτλο «Κρυφό», περιέχει δικές του μουσικές συνθέσεις πάνω στο κρητικό μοτίβο μουσικής με μαντινάδες από την συλλογή «Κρητικές Μαντινάδες» (1933) της λαογράφου Μαρίας Λιουδάκη. Στο άλμπουμ συμμετέχει στο λαγούτο και το τραγούδι ο Γιάννης Λιναρδάκης.",
+        "works-content-4": "Το 2024 κυκλοφόρησε ο δεύτερος του δίσκος με τίτλο «Άσπρουγας» που περιέχει έξι δικές του οργανικές συνθέσεις για κρητική λύρα, κόντρα μπάσο, βιολονστέλο. Στο άλμπουμ συμμετέχουν οι μουσικοί: Juliette Weiss, Στέφανος Φλωράς, Sol Ligertwood."
+    }
+};
+
+function translate(lang) {
+    document.querySelectorAll("[data-i18n]").forEach((el) => {
+        const key = el.getAttribute("data-i18n");
+        if (translations[lang] && translations[lang][key]) {
+            el.innerText = translations[lang][key];
+        }
+    });
+    document.documentElement.lang = lang;
+}
+
+document.getElementById("language-switcher-en").addEventListener("click", (e) => {
+    translate('en');
+});
+
+document.getElementById("language-switcher-fr").addEventListener("click", (e) => {
+    translate('fr');
+});
+
+document.getElementById("language-switcher-el").addEventListener("click", (e) => {
+    translate('el');
+});
+
+document.getElementById("language-switcher-mobile-en").addEventListener("click", (e) => {
+    translate('en');
+});
+
+document.getElementById("language-switcher-mobile-fr").addEventListener("click", (e) => {
+    translate('fr');
+});
+
+document.getElementById("language-switcher-mobile-el").addEventListener("click", (e) => {
+    translate('el');
+});
+
+// Set default language
+translate("en");
