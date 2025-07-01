20c3d7054cf97b91175e63190ff151e5098726f8
Charalampos Leontidis <chleontidis@yahoo.com>
2025-07-01 15:57:48 +0300

    Complete front page

--- js/eventsHandler.js
+++ js/eventsHandler.js
@@ -1,23 +1,45 @@
+function isTodayOrFuture(dateStr) {
+    if (dateStr) {
+        // Split the input string "dd/mm/yyyy"
+        const [day, month, year] = dateStr.split('/').map(Number);
+
+        // Create a Date object from the input (set to midnight)
+        const inputDate = new Date(year, month - 1, day);
+        inputDate.setHours(0, 0, 0, 0); // Normalize time to midnight
+
+        // Get today's date and normalize it to midnight
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        // Compare dates
+        return inputDate >= today;
+    } else {
+        return false;
+    }
+
+}
+
+
 function updateEventsPage(list) {
     let element = '';
     list.map(each => {
-        if (each.displayInEvents) {
+        if (isTodayOrFuture(each.date)) {
             element +=
                 `<div class='event-container'>
             <div class='event-title'>${each.title}</div>
             <div class='event-date'>${each.date}</div>
-            <div class='event-description'>${each.description}</div>
             </div>`;
         }
     });
-    document.getElementById('events-container').innerHTML = element;
-    document.getElementById('upcoming-container').innerHTML = element;
+    console.log(element);
+    if (element.length > 0) {
+        document.getElementById('events-container').innerHTML = element;
+    }
 }
 
 
 fetch('../events.json').then(response => response.json())
     .then(data => {
-        console.log(data);
         updateEventsPage(Object.values(data['en']));
         window.addEventListener('localeChange', e => {
             updateEventsPage(Object.values(data[e.detail]));
