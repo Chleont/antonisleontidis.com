67b7b16a11938119e5927da13e923397e1d63c4e
Charalampos Leontidis <chleontidis@yahoo.com>
2025-09-01 16:58:03 +0300

    add links to discography page

--- js/discographyPageHandler.js
+++ js/discographyPageHandler.js
@@ -10,15 +10,25 @@ function updateDiscographyPage(list) {
                 tracks += `<a class='album-track' target='_blank' href='${track.link}'><b>${track.title}</b></a>`;
             });
         }
+        let youtube = '';
+        let spotify = '';
+        if (each.youtube && each.youtube != null) {
+            youtube = `<a href=${each.youtube} target="_blank" class="social-youtube"><span
+                class="ti-youtube"></span></a>`;
+        }
+        if (each.spotify && each.spotify != null) {
+            spotify = `<a href=${each.spotify} target="_blank"
+                class="social-spotify"><span class="bi-spotify"></span></a>`;
+        }
         document.getElementById(each.id).innerHTML =
             `<div class='album-title'>${each.title}</div>
             <div class='album-date'>${each.date}</div>
             <div class='album-description'>${description}</div>
             <div class='album-contributors'>${contributors}</div>
-            <div class='album-tracks'>${tracks}</div>`;
+            <div class='album-tracks'>${tracks}</div>
+            `;
     });
 }
-
 
 fetch('../discography.json').then(response => response.json())
     .then(data => {
